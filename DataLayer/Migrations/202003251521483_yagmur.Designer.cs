8a46ac05b20464da385ce522968e21bdcbc65d01
yağmur <yagmurdapakli@gmail.com>
2020-04-02 23:15:12 +0300

    db alındı

--- DataLayer/Migrations/202003251521483_yagmur.Designer.cs
+++ DataLayer/Migrations/202003251521483_yagmur.Designer.cs
@@ -7,13 +7,13 @@ namespace DataLayer.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.4.0")]
-    public sealed partial class deneme : IMigrationMetadata
+    public sealed partial class yagmur : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(deneme));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(yagmur));
         
         string IMigrationMetadata.Id
         {
-            get { return "202003250027083_deneme"; }
+            get { return "202003251521483_yagmur"; }
         }
         
         string IMigrationMetadata.Source
