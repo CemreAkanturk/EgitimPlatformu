2513b068e9f0f83bf1e96fad11d126597091379f
yağmur <yagmurdapakli@gmail.com>
2020-05-26 17:45:52 +0300

    Calisan Firma

---
+++ EgitimPlatformu/Controllers/CalisanController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EgitimPlatformu.Controllers
+{
+    public class CalisanController : Controller
+    {
+        // GET: Calisan
+        public ActionResult CalisanDersler()
+        {
+            return View();
+        }
+        public ActionResult DersBilgileri()
+        {
+            return View();
+        }
+    }
+}
